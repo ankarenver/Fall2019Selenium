405eae75457911bf862587631c227c265e150423
ankarenver <1996ankarjian@gmail.com>
2020-03-01 13:29:50 -0500

    created basic navigation

---
+++ src/main/java/com/automation/test/day02/BasicNavigation.java
@@ -0,0 +1,35 @@
+package com.automation.test.day02;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class BasicNavigation {
+    public static void main(String[] args) {
+
+
+        // to stat selenium script we need:
+        // setup webdriver (browser driver) and create webdriver object
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver00 = new ChromeDriver();
+        WebDriver driver01 = new SafariDriver();
+        // in selenium everything starts from WebDriver interface
+        driver00.get("http://google.com");  // to open a website
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
