95e1759e487e35c7ac164c1e05d94638aaa4ed48
ankarenver <1996ankarjian@gmail.com>
2020-03-07 12:18:21 -0500

    created basic navigation

---
+++ src/main/java/com/automation/test/day05/RadioButtons.java
@@ -0,0 +1,35 @@
+package com.automation.test.day05;
+
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.F_driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class RadioButtons {
+    public static void main(String[] args) {
+
+        WebDriver driver = F_driver.getNewDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+        BrowserUtils.wait(2);
+        List<WebElement> radioButtons = driver.findElements(By.tagName("input"));
+        for (WebElement each : radioButtons){
+            if (each.isEnabled()){
+                each.click();
+                BrowserUtils.wait(2);
+                System.out.println("Clicking on: "+each.getAttribute("id"));
+            }else {
+                System.out.println("Button is not able to click, button is "+each.getAttribute("id"));
+            }
+        }
+        driver.close();
+
+
+
+
+
+    }
+}
