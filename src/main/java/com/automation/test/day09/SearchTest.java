f05b7d57da838bcfbda67e3daff2ec884cd71f1a
ankarenver <1996ankarjian@gmail.com>
2020-03-14 13:56:32 -0400

    TestNG

--- src/main/java/com/automation/test/day09/SearchTest.java
+++ src/main/java/com/automation/test/day09/SearchTest.java
@@ -1,7 +1,46 @@
 package com.automation.test.day09;
 
-public class SearchTest {
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.F_driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import java.util.List;
+
+public class SearchTest {
+    private WebDriver driver;
+
+
+    @BeforeMethod
+    public void setUp(){
+        // setup webdriver
+        driver = F_driver.getNewDriver("chrome");
+    }
+    @AfterMethod
+    public void teardown(){
+        driver.quit();
+    }
+
+
+    @Test
+    public void GoogleSearch(){
+        driver.get("https://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("java", Keys.ENTER);
+        BrowserUtils.wait(2);
+        List<WebElement> elements = driver.findElements(By.tagName("h3"));
+
+        for (WebElement each : elements){
+
+            if (each.getText().toLowerCase().contains("java")&&!each.getText().isEmpty()){
+                System.out.println(each.getText());
+            }
+        }
+    }
 
 
 }
